63c91b067f1d5778b33b320e149eab51410af1af
wittmane <ewittman@protonmail.com>
2024-09-09 23:03:50 -0500

    added import and export settings buttons

--- app/src/main/java/com/wittmane/testingedittext/settings/fragments/MainSettingsFragment.java
+++ app/src/main/java/com/wittmane/testingedittext/settings/fragments/MainSettingsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Eli Wittman
+ * Copyright (C) 2022-2024 Eli Wittman
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -16,16 +16,57 @@
 
 package com.wittmane.testingedittext.settings.fragments;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.wittmane.testingedittext.R;
+import com.wittmane.testingedittext.settings.IconUtils;
 
 public class MainSettingsFragment extends PreferenceFragment {
+    private static final String TAG = MainSettingsFragment.class.getSimpleName();
+
+    private View mView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference_screen_main);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+                             final Bundle savedInstanceState) {
+        mView = super.onCreateView(inflater, container, savedInstanceState);
+        return mView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
+        inflater.inflate(R.menu.settings, menu);
+
+        ActionBar actionBar = getActivity().getActionBar();
+        IconUtils.matchMenuIconColor(mView, menu, actionBar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final int itemId = item.getItemId();
+        if (itemId == R.id.action_import_settings) {
+            //TODO: (EW) implement
+            Log.d(TAG, "onOptionsItemSelected: import");
+        } else if (itemId == R.id.action_export_settings) {
+            //TODO: (EW) implement
+            Log.d(TAG, "onOptionsItemSelected: export");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
