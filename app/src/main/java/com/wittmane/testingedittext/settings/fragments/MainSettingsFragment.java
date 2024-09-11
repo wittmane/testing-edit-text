62e8b6d7726da44cf2c316f547731512c666ef03
wittmane <ewittman@protonmail.com>
2024-09-10 23:07:38 -0500

    added functionality to read and write to a file

--- app/src/main/java/com/wittmane/testingedittext/settings/fragments/MainSettingsFragment.java
+++ app/src/main/java/com/wittmane/testingedittext/settings/fragments/MainSettingsFragment.java
@@ -17,7 +17,11 @@
 package com.wittmane.testingedittext.settings.fragments;
 
 import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.preference.PreferenceFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,12 +30,27 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.wittmane.testingedittext.R;
 import com.wittmane.testingedittext.settings.IconUtils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainSettingsFragment extends PreferenceFragment {
     private static final String TAG = MainSettingsFragment.class.getSimpleName();
+
+    private static final int EXPORT_SETTINGS_FILE = 1;
+    private static final int IMPORT_SETTINGS_FILE = 2;
+    private static final String SETTINGS_FILE_MIME_TYPE = "text/plain";
 
     private View mView;
 
@@ -61,12 +80,94 @@ public class MainSettingsFragment extends PreferenceFragment {
     public boolean onOptionsItemSelected(final MenuItem item) {
         final int itemId = item.getItemId();
         if (itemId == R.id.action_import_settings) {
-            //TODO: (EW) implement
-            Log.d(TAG, "onOptionsItemSelected: import");
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType(SETTINGS_FILE_MIME_TYPE);
+            startActivityForResult(intent, IMPORT_SETTINGS_FILE);
         } else if (itemId == R.id.action_export_settings) {
-            //TODO: (EW) implement
-            Log.d(TAG, "onOptionsItemSelected: export");
+            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType(SETTINGS_FILE_MIME_TYPE);
+            String instant = new SimpleDateFormat("yyyyMMddhhmmss", Locale.US)
+                    .format(Calendar.getInstance().getTime());
+            intent.putExtra(Intent.EXTRA_TITLE, "TestingEditTextSettings-" + instant + ".txt");
+            startActivityForResult(intent, EXPORT_SETTINGS_FILE);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+        if (resultCode == Activity.RESULT_CANCELED) {
+            return;
+        }
+        Uri uri = null;
+        if (resultData != null) {
+            uri = resultData.getData();
+        }
+        if (resultCode != Activity.RESULT_OK || uri == null) {
+            Toast.makeText(getActivity(), getActivity().getString(R.string.failed_to_access_file),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        if (requestCode == IMPORT_SETTINGS_FILE) {
+            if (!importSettings(uri)) {
+                Toast.makeText(getActivity(),
+                        getActivity().getString(R.string.failed_to_import_settings),
+                        Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == EXPORT_SETTINGS_FILE) {
+            if (!exportSettings(uri)) {
+                Toast.makeText(getActivity(),
+                        getActivity().getString(R.string.failed_to_export_settings),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean importSettings(Uri uri) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                     getActivity().getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found for importing: " + e.getMessage());
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read file: " + e.getMessage());
+            return false;
+        }
+        //TODO: (EW) actually import the data
+        Log.d(TAG, "File contents: " + stringBuilder.toString());
+        return true;
+    }
+
+    private boolean exportSettings(Uri uri) {
+        try (ParcelFileDescriptor pfd =
+                     getActivity().getContentResolver(). openFileDescriptor(uri, "w")) {
+            if (pfd == null) {
+                Log.e(TAG, "Parcel file descriptor is null");
+                return false;
+            }
+            try (FileOutputStream fileOutputStream =
+                         new FileOutputStream(pfd.getFileDescriptor())) {
+                //TODO: (EW) export real data
+                fileOutputStream.write(
+                        ("Written at " + System.currentTimeMillis() + "\n").getBytes());
+            }
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found for exporting: " + e.getMessage());
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to file: " + e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
